ba61655f100f9070dc7580660985e0a1667901bf
Alexander Hörtenhuber <alex.hoertenhuber@gmx.at>
2025-02-17 10:11:56 +0100

    upload image button, getContent from RAM, getNotes from RAM, replace appendFile with writeFile

--- test/tryout.js
+++
@@ -1,9 +0,0 @@
-import { getContent } from '../model.js';
-
-    let routes = []
-        let content = JSON.parse(getContent());
-        for(let page of content.pages) {
-            routes.push({ path: page.path, label: page.title })
-        }
-    console.log(routes[0])
-    console.log(routes)
